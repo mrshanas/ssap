76ee5f2a0235539707a2472c254ac511422e0d1b
elirehema <rocksandystone@gmail.com>
2022-07-23 03:17:36 +0300

    List all accounts

--- mixins/mixins.js
+++ mixins/mixins.js
@@ -4,72 +4,9 @@ Vue.mixin({
 
   layout: 'default',
   data: function () {
-
-    return {
-      months_labels: [
-        "Jan",
-        "Feb",
-        "Mar",
-        "Apr",
-        "May",
-        "Jun",
-        "Jul",
-        "Aug",
-        "Sept",
-        "Oct",
-        "Nov",
-        "Dec",
-      ],
-      week_labels: [
-        'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'
-      ],
-      publicPath: window.location.origin + '/lib',
-      itemsperpage: 15,
-      totalvisible: 7,
-      delay_seconds: 2000,
-      toggle_exclusive: 2,
-      body: {
-        options: {
-          page: 1,
-          itemsPerPage: 15,
-          sortBy: ["name"]
-        }
-      },
-      thumbnil: 'https://avataaars.io/?avatarStyle=Circle&topType=ShortHairSides&accessoriesType=Prescription02&hairColor=BrownDark&facialHairType=Blank&clotheType=BlazerSweater&eyeType=Default&eyebrowType=UnibrowNatural&mouthType=Twinkle&skinColor=Light',
-   
-    }
+    return {}
   },
   methods: {
-    async fetchclientdata(id) {
-      return await this.$api
-        .$get(`patients/${id}/`)
-        .then(response => {
-          this.userdata = response;
-        })
-        .catch(error => {
-          console.log(error);
-        });
-    },
-    async getPatientDiagnoses(id) {
-      return await this.$api.$get(`diagnoses/${id}/`)
-        .then(response => {
-          this.diagnoses = response.reverse();
-        }).catch(error => {
-          console.log(error);
-
-        });
-    },
-
-    row_classes(item) {
-      if (item.isReversed) {
-        return "strikeout";
-      }
-    },
-
-    hasPermission(p) {
-      //console.log(this.hasAnyPermission(this.$store.getters.thisuserpermissions, p))
-      return this.hasAnyPermission(this.$store.getters.thisuserpermissions, p);
-    },
     formatPrice(value) {
       if (value != null) {
         return value.toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1,');
@@ -96,29 +33,6 @@ Vue.mixin({
   computed: {
     isMdAndUp() {
       return this.$vuetify.breakpoint.mdAndUp;
-    },
-    userinfo() {
-      return this.$store.getters.userInfos;
-    },
-    username() {
-      return this.userinfo.username;
-    },
-    email() {
-      return this.userinfo.email;
-    },
-    domain() {
-      return window.location.hostname === "app.ospicx.com" || "localhost";
-    },
-    isfinance() {
-      return this.$root.context.from.name.startsWith(prefix)
-    },
-    filebaseUri() {
-      var url = this.$api.defaults.baseURL;
-      var path = url.endsWith("/") ? url.slice(0, -1) : url;
-      return path;
-    },
-    thumbnail() {
-      return this.profileimage == null ? this.thumbnil : this.filebaseUri + this.profileimage;
     },
     ...mapGetters({
       clientId: "clientId"
--- pages/accounts.vue
+++ pages/accounts.vue
@@ -1,70 +1,54 @@
 <template>
   <v-container class="fill-height pa-0" fluid>
-    <v-tabs
-      v-model="tab"
-      background-color="primary"
-      color="basil" 
-      grow
-    >
-      <v-tab  class="font-weight-bold  text-subtitle-1">SAVINGS</v-tab>
-       <v-tab class="font-weight-bold  text-subtitle-1">LOAN</v-tab>
-        <v-tab class="font-weight-bold  text-subtitle-1">SHARES</v-tab>
+    <v-tabs v-model="tab" background-color="primary" color="basil" grow>
+      <v-tab class="font-weight-bold text-subtitle-1">SAVINGS</v-tab>
+      <v-tab class="font-weight-bold text-subtitle-1">LOAN</v-tab>
+      <v-tab class="font-weight-bold text-subtitle-1">SHARES</v-tab>
     </v-tabs>
 
-    <v-tabs-items v-model="tab">
-      <v-tab-item>
-        <tab-savingaccounts :saviingaccounts="savingaccounts"></tab-savingaccounts>
-         
+    <v-tabs-items style="width: 100% !important" v-model="tab">
+      <v-tab-item style="width: 100% !important">
+        <tab-savingaccounts
+          :savingaccounts="savingaccounts"
+        ></tab-savingaccounts>
       </v-tab-item>
-      <v-tab-item>
-        <v-card
-          color="basil"
-          flat
-        >
-          <v-card-text>LOANS</v-card-text>
-        </v-card>
+      <v-tab-item style="width: 100% !important">
+        <tab-loanaccounts :loanaccounts="loanaccounts"></tab-loanaccounts>
       </v-tab-item>
-      <v-tab-item>
-        <v-card
-          color="basil"
-          flat
-        >
-          <v-card-text>SHARE</v-card-text>
-        </v-card>
+      <v-tab-item style="width: 100% !important">
+        <tab-shareccounts :shareaccounts="shareaccounts"></tab-shareccounts>
       </v-tab-item>
     </v-tabs-items>
-</v-container>
-
+  </v-container>
 </template>
 
 <script>
-import SavingAccountTab from "@/components/accounts/savings.vue"
+import SavingAccountTab from "@/components/accounts/savings.vue";
+import LoanAccountTab from "@/components/accounts/loans.vue";
+import SharesAccountTab from "@/components/accounts/shares.vue";
 import { mapGetters } from "vuex";
-  export default {
-    components:{
-      "tab-savingaccounts": SavingAccountTab
-    },
-    data () {
-      return {
-        tab: null,
-         }
-    },
-    created(){
-      console.log(this.clientId);
-      this.$store.dispatch("_getaccounts", this.clientId);
-    },
-    computed:{
+export default {
+  components: {
+    "tab-savingaccounts": SavingAccountTab,
+    "tab-loanaccounts": LoanAccountTab,
+    "tab-shareccounts": SharesAccountTab,
+  },
+  data() {
+    return {
+      tab: null,
+    };
+  },
+  created() {
+    console.log(this.clientId);
+    this.$store.dispatch("_getaccounts", this.clientId);
+  },
+  computed: {
     ...mapGetters({
-       savingaccounts: "savingaccounts"
+      savingaccounts: "savingaccounts",
+      loanaccounts: "loanaccounts",
+      shareaccounts: "shareaccounts",
     }),
-    }
-  };
+  },
+};
 </script>
-<style>
-#app {
-  font-family: "Lato", sans-serif;
-}
-.routeLink {
-  text-decoration: none;
-}
-</style>
+
