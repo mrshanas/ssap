4f980c438f522299be70514ba3e329e8f780b436
elirehema <rocksandystone@gmail.com>
2022-07-26 17:54:35 +0300

    Add share buttons

--- pages/share.vue
+++ pages/share.vue
@@ -1,20 +1,29 @@
 <template>
-  <v-container class="fill-height">
-    <v-row align="center" justify="center" class="fill-height">
-        <p>Under development</p>
+  <v-container class="fill-height" style="background-color: transparent">
+    <v-row align="center" class="d-flex justify-space-around">
+      <v-col cols="12" sm="6">
+        <div class="text-center">
+          <div class="my-2">
+            <v-btn color="blue" fab x-small dark>
+              <v-icon>mdi-twitter</v-icon>
+            </v-btn>
+            <v-btn color="blue" fab x-small dark>
+              <v-icon>mdi-instagram</v-icon>
+            </v-btn>
+            <v-btn color="blue" fab x-small dark>
+              <v-icon>mdi-facebook</v-icon>
+            </v-btn>
+            <v-btn color="blue" fab x-small dark>
+              <v-icon>mdi-linkedin</v-icon>
+            </v-btn>
+            <v-btn color="blue" fab x-small dark>
+              <v-icon>mdi-email-outline</v-icon>
+            </v-btn>
+          </div>
+        </div>
+      </v-col>
     </v-row>
-</v-container>
-
+  </v-container>
 </template>
 
 <script>
-export default {};
-</script>
-<style>
-#app {
-  font-family: "Lato", sans-serif;
-}
-.routeLink {
-  text-decoration: none;
-}
-</style>
