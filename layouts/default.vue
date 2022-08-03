bfe01f65d5453874984dff55defb29865516a604
shanas <87828025+mrshanas@users.noreply.github.com>
2022-08-03 13:50:18 +0300

    Added footer to the default layout
    I have added the footer and responsive styles for the third party transfer form

--- layouts/default.vue
+++ layouts/default.vue
@@ -132,6 +132,15 @@
     <v-main>
       <v-container style="background-color: white" class="ma-0 pa-0" fluid>
         <nuxt />
+        <v-container
+          class="d-flex align-center justify-space-between px-3 py-5"
+        >
+          <p class="pt-3">Contact Us</p>
+          <div>
+            <p class="pa-0 ma-0">+255 737 789 207</p>
+            <p>info@amala.co.tz</p>
+          </div>
+        </v-container>
       </v-container>
     </v-main>
   </v-app>
@@ -352,4 +361,3 @@ export default {
   },
 };
 </script>
-
--- pages/index.vue
+++ pages/index.vue
@@ -186,13 +186,6 @@
         </v-col>
       </v-row>
     </v-card>
-    <v-container class="d-flex align-center justify-space-between px-3 py-5">
-      <p class="pt-3">Contact Us</p>
-      <div>
-        <p class="pa-0 ma-0">+255 737 789 207</p>
-        <p>info@amala.co.tz</p>
-      </div>
-    </v-container>
   </v-container>
 </template>
 
