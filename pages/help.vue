d13b065d28ee68c40494a8320d89145edb394b75
Revaycolizer <albinuslucas@gmail.com>
2022-08-11 10:03:23 +0300

    og

--- pages/help.vue
+++ pages/help.vue
@@ -9,21 +9,77 @@
             <div class="text-h4">Frequently Asked Questions</div>
           </div>
           <v-col cols="16"></v-col>
-          <v-list>
-            <v-list-group color="dlue" v-for="item in items" :key="item.title">
-              <template v-slot:activator>
-                <v-list-item-content>
-                  <v-list-item-title class="text-h6" color="dlue" v-text="item.title"></v-list-item-title>
-                </v-list-item-content>
-              </template>
 
-              <v-list-item color="dlue" v-for="child in item.items" :key="child.title">
-                <v-list-item-content>
-                  <v-list-item-title class="text-h6" color="dlue" v-text="child.title"></v-list-item-title>
-                </v-list-item-content>
-              </v-list-item>
-            </v-list-group>
-          </v-list>
+
+          <v-expansion-panels>
+            <v-expansion-panel>
+              <v-expansion-panel-header class="text-h5">
+                How do I apply for new loan account?
+                <template v-slot:actions>
+                  <v-icon color="dlue">
+                    $expand
+                  </v-icon>
+                </template>
+              </v-expansion-panel-header>
+              <v-expansion-panel-content>
+                <div class="text-h6">
+                  To apply for loan account, click on " Apply for Loan " given on the Side Navigation</div>
+              </v-expansion-panel-content>
+            </v-expansion-panel>
+          </v-expansion-panels>
+
+
+          <v-expansion-panels>
+            <v-expansion-panel>
+              <v-expansion-panel-header class="text-h5">
+                Where can I view my profile information?
+                <template v-slot:actions>
+                  <v-icon color="dlue">
+                    $expand
+                  </v-icon>
+                </template>
+              </v-expansion-panel-header>
+              <v-expansion-panel-content>
+                <div class="text-h6">
+                  You can view your profile information by clicking on the User icon at the top
+                  right position</div>
+              </v-expansion-panel-content>
+            </v-expansion-panel>
+          </v-expansion-panels>
+
+          <v-expansion-panels>
+            <v-expansion-panel>
+              <v-expansion-panel-header class="text-h5">Where can I see my Savings Account Transactions?<template
+                  v-slot:actions>
+                  <v-icon color="primary">
+                    $expand
+                  </v-icon>
+                </template>
+              </v-expansion-panel-header>
+              <v-expansion-panel-content>
+                <div class="text-h6">
+                  To view your savings account transactions, navigate to the Account Sections, the
+                  Savings and click on the Saving Accounts</div>
+              </v-expansion-panel-content>
+            </v-expansion-panel>
+          </v-expansion-panels>
+
+
+          <v-expansion-panels>
+            <v-expansion-panel>
+              <v-expansion-panel-header class="text-h5">How to make a payment for Loan Account?<template v-slot:actions>
+                  <v-icon color="primary">
+                    $expand
+                  </v-icon>
+                </template>
+              </v-expansion-panel-header>
+              <v-expansion-panel-content>
+                <div class="text-h6">
+                  To make a payment for a loan account, navigate to the Accounts sections, LOAN then
+                  open your required Loan Account and click on Make Payment option</div>
+              </v-expansion-panel-content>
+            </v-expansion-panel>
+          </v-expansion-panels>
 
         </v-card>
       </v-col>
@@ -33,35 +89,6 @@
 
 <script>
 export default {
-  data: () => ({
-    items: [
-      {
-
-        items: [{ title: 'To apply for loan account, click on " Apply for Loan " given on the Side Navigation' }],
-        title: 'How do I apply for new loan account?',
-      },
-      {
-
-
-        items: [
-          { title: 'You can view your profile information by clicking on the User icon at the top right position' },
-        ],
-        title: 'Where can I view my profile information?',
-      },
-      {
-
-        items: [{ title: 'To view your savings account transactions, navigate to the Account Sections, the Savings and click on the Saving Accounts' }],
-        title: 'Where can I see my Savings Account Transactions?',
-      },
-      {
-
-        items: [{ title: 'To make a payment for a loan account, navigate to the Accounts sections, LOAN then open your required Loan Account and click on Make Payment option' }],
-        title: 'How to make a payment for Loan Account?',
-      },
-
-    ]
-  })
-
 
 };
 </script>
